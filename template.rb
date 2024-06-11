bbe8d357bf44e156a98993f38e26cf72eecdeaa7
banias <ext-andrii.banias@here.com>
2024-06-11 17:58:30 +0300

    Update variable name in the template.rb

--- template.rb
+++ template.rb
@@ -1,8 +1,8 @@
 class Maps < Formula
   desc "With the Command Line Interface you can access HERE Map Making service from a command line or with scripts."
-  url "https://artifact.api.platform.here.com/v1/references/hrn:here:artifact:::com.here.platform:cli:${OLP_CLI_VERSION}/maps-cli-${OLP_CLI_VERSION}.tar.gz\?apiKey\=${ARTIFACT_SERVICE_API_KEY}"
+  url "https://artifact.api.platform.here.com/v1/references/hrn:here:artifact:::com.here.platform:cli:${MAPS_CLI_VERSION}/maps-cli-${MAPS_CLI_VERSION}.tar.gz\?apiKey\=${ARTIFACT_SERVICE_API_KEY}"
   sha256 "${SHA256}"
-  version "${OLP_CLI_VERSION}"
+  version "${MAPS_CLI_VERSION}"
   license "Proprietary"
 
   depends_on "openjdk@17"
@@ -10,7 +10,7 @@ class Maps < Formula
   def install
     prefix.install "HERE_NOTICE"
     prefix.install "LICENSE"
-    libexec.install "cli${SCALA_SUFFIX}-${OLP_CLI_VERSION}.jar"
+    libexec.install "cli${SCALA_SUFFIX}-${MAPS_CLI_VERSION}.jar"
     # There are two versions of variables: build time and runtime
     # JAVA_VERSION, ALLOW_DEEP_REFLECTION, 2 and @ needs to be preserved during build time by using § instead of $
     (bin/"maps").write <<~EOS
@@ -21,7 +21,7 @@ class Maps < Formula
       if ! [[ "§JAVA_VERSION" =~ ^1"."+ ]]; then
         ALLOW_DEEP_REFLECTION="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens=java.base/sun.security.util=ALL-UNNAMED"
       fi
-      exec "§{JAVA_HOME}/bin/java" §{ALLOW_DEEP_REFLECTION:-} -Dfile.encoding=UTF8 -cp "#{libexec}/cli${SCALA_SUFFIX}-${OLP_CLI_VERSION}.jar" "com.here.platform.cli.MapsMain" "§@"
+      exec "§{JAVA_HOME}/bin/java" §{ALLOW_DEEP_REFLECTION:-} -Dfile.encoding=UTF8 -cp "#{libexec}/cli${SCALA_SUFFIX}-${MAPS_CLI_VERSION}.jar" "com.here.platform.cli.MapsMain" "§@"
     EOS
   end
 
