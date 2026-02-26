492ad8fdcca4c0a79c3fd295ba7b397919271284
Dmitry Abramov <47140652+dmitriy-abramov@users.noreply.github.com>
2026-02-26 07:16:43 +0000

    Add version 18.0.32

--- Formula/maps.rb
+++ Formula/maps.rb
@@ -1,8 +1,8 @@
 class Maps < Formula
   desc "With the Command Line Interface you can access HERE Map Making service from a command line or with scripts."
-  url "https://github.com/heremaps/homebrew-map-making-cli/releases/download/18.0.27/maps_cli_2.13-18.0.27.tar.gz"
-  sha256 "665bd7db44e3364f77fc4d9ee6f656d4ba7af906c3460e84e64591f1c6a031a3"
-  version "18.0.27"
+  url "https://github.com/heremaps/homebrew-map-making-cli/releases/download/18.0.32/maps_cli_2.13-18.0.32.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "18.0.32"
   license "Proprietary"
 
   depends_on "openjdk@17"
@@ -10,7 +10,7 @@ class Maps < Formula
   def install
     prefix.install "HERE_NOTICE"
     prefix.install "LICENSE"
-    libexec.install "cli_2.13-18.0.27.jar"
+    libexec.install "cli_2.13-18.0.32.jar"
     # There are two versions of variables: build time and runtime
     # JAVA_VERSION, ALLOW_DEEP_REFLECTION, 2 and @ needs to be preserved during build time by using $ instead of $
     (bin/"maps").write <<~EOS
@@ -21,7 +21,7 @@ class Maps < Formula
       if ! [[ "$JAVA_VERSION" =~ ^1"."+ ]]; then
         ALLOW_DEEP_REFLECTION="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens=java.base/sun.security.util=ALL-UNNAMED"
       fi
-      exec "${JAVA_HOME}/bin/java" ${ALLOW_DEEP_REFLECTION:-} -Dfile.encoding=UTF8 -cp "#{libexec}/cli_2.13-18.0.27.jar" "com.here.platform.cli.MapsMain" "$@"
+      exec "${JAVA_HOME}/bin/java" ${ALLOW_DEEP_REFLECTION:-} -Dfile.encoding=UTF8 -cp "#{libexec}/cli_2.13-18.0.32.jar" "com.here.platform.cli.MapsMain" "$@"
     EOS
   end
 
